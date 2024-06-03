cf8c661dee7e92fa2af7d1066df176eb807d4947
Carla Cairns <carla.cairns@expeditors.com>
2024-06-03 15:51:09 -0500

    track service impl methods

--- TrackApplication/src/main/java/org/edwinsoto/trackapplication/service/impl/JpaTrackServiceImpl.java
+++ TrackApplication/src/main/java/org/edwinsoto/trackapplication/service/impl/JpaTrackServiceImpl.java
@@ -6,6 +6,7 @@ import org.edwinsoto.trackapplication.service.JpaTrackService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,32 +21,44 @@ public class JpaTrackServiceImpl implements JpaTrackService {
     }
 
     @Override
-    public JpaTrack save(JpaTrack jpaAdopter) {
-        return null;
+    public JpaTrack save(JpaTrack jpaTrack) {
+        return jpaTrackRepository.save(jpaTrack);
     }
 
     @Override
     public List<JpaTrack> findAll() {
-        return null;
+        return jpaTrackRepository.findAll();
     }
 
     @Override
     public Optional<JpaTrack> findOne(Integer id) {
-        return Optional.empty();
+        return jpaTrackRepository.findById(id);
     }
 
     @Override
     public boolean isExists(Integer id) {
-        return false;
+        return jpaTrackRepository.existsById(id);
     }
 
     @Override
     public JpaTrack updateTrack(Integer id, JpaTrack jpaTrack) {
-        return null;
+        jpaTrack.setId(id);
+        jpaTrack.setModifiedDate(LocalDate.now());
+        return jpaTrackRepository.findById(id).map(toBeUpdated ->{
+            Optional.ofNullable(jpaTrack.getTitle()).ifPresent(toBeUpdated::setTitle);
+            Optional.ofNullable(jpaTrack.getAlbum()).ifPresent(toBeUpdated::setAlbum);
+            Optional.ofNullable(jpaTrack.getIssueDate()).ifPresent(toBeUpdated::setIssueDate);
+            Optional.ofNullable(jpaTrack.getDurationSec()).ifPresent(toBeUpdated::setDurationSec);
+            Optional.ofNullable(jpaTrack.getAudioType()).ifPresent(toBeUpdated::setAudioType);
+            Optional.ofNullable(jpaTrack.getGenre()).ifPresent(toBeUpdated::setGenre);
+            Optional.ofNullable(jpaTrack.getArtists()).ifPresent(toBeUpdated::setArtists);
+
+            return jpaTrackRepository.save(toBeUpdated);
+        }).orElseThrow(()-> new RuntimeException("Track does not exist"));
     }
 
     @Override
     public void delete(Integer id) {
-
+        jpaTrackRepository.deleteById(id);
     }
 }
