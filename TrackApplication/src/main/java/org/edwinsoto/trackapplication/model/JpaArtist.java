9cb0736260b09af46cc5bd9312e658d897ad6afa
Carla Cairns <carla.cairns@expeditors.com>
2024-06-03 09:26:33 -0500

    base for artist entity

--- TrackApplication/src/main/java/org/edwinsoto/trackapplication/model/JpaArtist.java
+++ TrackApplication/src/main/java/org/edwinsoto/trackapplication/model/JpaArtist.java
@@ -1,4 +1,35 @@
 package org.edwinsoto.trackapplication.model;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@RequiredArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table("artists")
 public class JpaArtist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NonNull
+    private String name;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate dateOfBirth;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate registerDate;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate modifiedData;
+
 }
