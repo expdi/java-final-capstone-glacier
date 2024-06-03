01693342edd19562a9f516223a7ef04747c9c1ea
Juan de Dios Hernandez <juandedios.hernandez@expeditors.com>
2024-06-03 14:46:06 -0500

    feat: Avance de obtener el precio del Track con autenticacion ✨

--- TrackApplication/src/main/java/org/edwinsoto/trackapplication/dao/NetworkPricingDAO.java
+++ TrackApplication/src/main/java/org/edwinsoto/trackapplication/dao/NetworkPricingDAO.java
@@ -1,7 +1,10 @@
 package org.edwinsoto.trackapplication.dao;
 
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.edwinsoto.trackapplication.model.Track;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestClient;
@@ -14,15 +17,29 @@ public class NetworkPricingDAO implements PricingDAO {
 
     private String pricingURL;
 
+    @Autowired
+    private Environment env;
+
     public NetworkPricingDAO() {
         var baseUrl = "http://localhost:8081";
         var endpoint = "api/v1/pricing";
         pricingURL = endpoint + "/id={id}";
 
+        String userName = env.getProperty("SPRING_ADMIN_USERNAME");
+        String password = env.getProperty("SPRING_ADMIN_PASSWORD");
+        String plainCreds = userName + ":" + password;
+        byte[] plainCredsBytes = plainCreds.getBytes();
+        byte[] base64CredsBytes = Base64.encodeBase64(plainCredsBytes, true);
+        String base64Creds = new String(base64CredsBytes);
+
         this.restClient = RestClient.builder()
                 .baseUrl(baseUrl)
+                .defaultHeader("Authorization", "Basic " + base64Creds)
+
                 .defaultHeader("Accept", "application/json")
                 .defaultHeader("Content-Type", "application/json")
+                .defaultHeader("Content-Type", userName)
+                .defaultHeader("Content-Type", password)
                 .build();
     }
 
