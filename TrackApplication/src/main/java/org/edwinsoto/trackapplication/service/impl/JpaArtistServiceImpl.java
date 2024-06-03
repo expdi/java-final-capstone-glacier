4a61bc0df83fe7fb0c1745c86268aff51c97346c
Carla Cairns <carla.cairns@expeditors.com>
2024-06-03 15:12:13 -0500

    service impls implement service interfaces

--- TrackApplication/src/main/java/org/edwinsoto/trackapplication/service/impl/JpaArtistServiceImpl.java
+++ TrackApplication/src/main/java/org/edwinsoto/trackapplication/service/impl/JpaArtistServiceImpl.java
@@ -1,4 +1,41 @@
 package org.edwinsoto.trackapplication.service.impl;
 
-public class JpaArtistServiceImpl {
+import org.edwinsoto.trackapplication.model.JpaArtist;
+import org.edwinsoto.trackapplication.repository.JpaArtistRepository;
+import org.edwinsoto.trackapplication.service.JpaArtistService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Profile("jpa")
+@Service
+public class JpaArtistServiceImpl implements JpaArtistService {
+
+    private JpaArtistRepository jpaArtistRepository;
+
+    public JpaArtistServiceImpl(JpaArtistRepository jpaArtistRepository) {
+        this.jpaArtistRepository = jpaArtistRepository;
+    }
+
+    @Override
+    public JpaArtist save(JpaArtist jpaAdopter) {
+        return null;
+    }
+
+    @Override
+    public List<JpaArtist> findAll() {
+        return null;
+    }
+
+    @Override
+    public Optional<JpaArtist> findOne(Integer id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean isExists(Integer id) {
+        return false;
+    }
 }
--- TrackApplication/src/main/java/org/edwinsoto/trackapplication/service/impl/JpaTrackServiceImpl.java
+++ TrackApplication/src/main/java/org/edwinsoto/trackapplication/service/impl/JpaTrackServiceImpl.java
@@ -1,4 +1,41 @@
 package org.edwinsoto.trackapplication.service.impl;
 
-public class JpaTrackServiceImpl {
+import org.edwinsoto.trackapplication.model.JpaTrack;
+import org.edwinsoto.trackapplication.repository.JpaTrackRepository;
+import org.edwinsoto.trackapplication.service.JpaTrackService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Profile("jpa")
+@Service
+public class JpaTrackServiceImpl implements JpaTrackService {
+
+    private JpaTrackRepository jpaTrackRepository;
+
+    public JpaTrackServiceImpl(JpaTrackRepository jpaTrackRepository) {
+        this.jpaTrackRepository = jpaTrackRepository;
+    }
+
+    @Override
+    public JpaTrack save(JpaTrack jpaAdopter) {
+        return null;
+    }
+
+    @Override
+    public List<JpaTrack> findAll() {
+        return null;
+    }
+
+    @Override
+    public Optional<JpaTrack> findOne(Integer id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean isExists(Integer id) {
+        return false;
+    }
 }
