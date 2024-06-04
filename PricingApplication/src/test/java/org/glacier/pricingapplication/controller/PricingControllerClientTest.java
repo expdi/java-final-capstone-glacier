d81e3de46e9a2bcb73a96f0e581ad62e1404c596
Jesus Cortez <jesus.cortezvaldez@expeditors.com>
2024-06-04 16:32:21 -0500

    Test for pricing using credentials added

--- PricingApplication/src/test/java/org/glacier/pricingapplication/controller/PricingControllerClientTest.java
+++ PricingApplication/src/test/java/org/glacier/pricingapplication/controller/PricingControllerClientTest.java
@@ -3,19 +3,25 @@ package org.glacier.pricingapplication.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.annotation.PostConstruct;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestClient;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.util.Base64;
+import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
@@ -31,15 +37,23 @@ public class PricingControllerClientTest {
 
     private RestClient restClient;
 
-    private String baseUrl;
-    private String rootUrl;
-    private String pricingURL;
+    @Value("${env.user.superuser}")
+    private String superUser; // = env.getProperty("env.user.superuser");
+
+    @Value("${env.user.superuserpassword}")
+    private String superUserPassword ;//= System.getenv("${env.user.superuserpassword}");
+
+    @Value("${env.user.regularuser}")
+    private String regularUser;// = System.getenv("${env.user.regularuser}");
+
+    @Value("${env.user.regularuserpassword}")
+    private String regularUserPassword;// = System.getenv("${env.user.regularuserpassword}");
+
 
     @PostConstruct
     public void init() {
         var baseUrl = "http://localhost:" + port;
         var endpoint = "api/v1/pricing";
-        pricingURL = endpoint + "/id={id}";
 
         //TODO: Obtaining values from environment variables
         String userName = "regularUser"; //env.getProperty("env.user.name");
@@ -58,7 +72,7 @@ public class PricingControllerClientTest {
     @Test
     public void getPricingForTrackTest() throws IOException {
 
-        ResponseEntity<Integer> response = getRestClient("regularUser", "password2").get()
+        ResponseEntity<Integer> response = getRestClient(this.regularUser, this.regularUserPassword).get()
                 .uri("api/v1/pricing/id={id}", 1).retrieve()
                 .toEntity(Integer.class);
 
@@ -66,8 +80,9 @@ public class PricingControllerClientTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"superUser, password1", "regularUser, password2"})
-    public void getPricingForTrackMultipleUsers(String user, String password) throws IOException {
+    //@CsvSource(generateCsvSourceWithCredential(this.superUser, this.superUserPassword, this.regularUser, this.regularUserPassword))
+    @MethodSource("generateCsvSourceWithCredential")
+        public void getPricingForTrackMultipleUsers(String user, String password) throws IOException {
 
         ResponseEntity<Integer> response = getRestClient(user, password).get()
                 .uri("api/v1/pricing/id={id}", 1).retrieve()
@@ -90,7 +105,7 @@ public class PricingControllerClientTest {
     @Test
     public void updateLimitForPricingByAuthorizedUser() throws IOException {
 
-        ResponseEntity<?> response = getRestClient("superUser", "password1").put()
+        ResponseEntity<?> response = getRestClient(this.superUser, this.superUserPassword).put()
                 .uri("/api/v1/pricing/setLimits/{lowerLimit}/{upperLimit}", 1, 10).retrieve()
                 .toEntity(ResponseEntity.class);
 
@@ -101,7 +116,7 @@ public class PricingControllerClientTest {
     public void updateLimitForPricingByUnauthorizedUser() throws IOException {
 
         assertThrows(HttpClientErrorException.Forbidden.class, () -> {
-            ResponseEntity<?> response = getRestClient("regularUser", "password2").put()
+            ResponseEntity<?> response = getRestClient(this.regularUser, this.regularUserPassword).put()
                     .uri("/api/v1/pricing/setLimits/{lowerLimit}/{upperLimit}", 1, 10).retrieve()
                     .toEntity(ResponseEntity.class);;
         });
@@ -112,7 +127,7 @@ public class PricingControllerClientTest {
 
 
         assertThrows(HttpClientErrorException.BadRequest.class, () -> {
-            ResponseEntity<?> response = getRestClient("superUser", "password1").put()
+            ResponseEntity<?> response = getRestClient(this.superUser, this.superUserPassword).put()
                     .uri("/api/v1/pricing/setLimits/{lowerLimit}/{upperLimit}", 10, 10).retrieve()
                     .toEntity(ResponseEntity.class);
         });
@@ -121,7 +136,6 @@ public class PricingControllerClientTest {
     private RestClient getRestClient(String userName, String password) {
         var baseUrl = "http://localhost:" + port;
         var endpoint = "api/v1/pricing";
-        pricingURL = endpoint + "/id={id}";
 
         String valueToEncode = userName + ":" + password;
         String base64Creds = Base64.getEncoder().encodeToString(valueToEncode.getBytes());
@@ -132,4 +146,19 @@ public class PricingControllerClientTest {
                 .defaultHeader("Content-Type", "application/json")
                 .build();
     }
+
+    //private static  String[][] generateCsvSourceWithCredential() {
+    private static String[][] generateCsvSourceWithCredential(String superUser, String superUserPassword, String regularUser, String regularUserPassword) {
+        return new String[][] {
+                { superUser, superUserPassword },
+                { regularUser, regularUserPassword}
+        };
+    }
+
+//    static Stream<Arguments> generateCsvSourceWithCredential() {
+//        return Stream.of(
+//                Arguments.of( System.getenv("env.user.superuser"), System.getenv("env.user.superuserpassword"))
+//                //,Arguments.of( "${env.user.regularuser}", "password2")
+//        );
+//    }
 }
